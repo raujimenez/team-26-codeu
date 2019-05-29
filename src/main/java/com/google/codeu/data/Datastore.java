bd9892bc415be8fe9b9d5840a0a3cb26f025004f
Benjamín Valdez <benjamin.vr.1998@gmail.com>
2019-05-29 00:42:17 -0500

    Added a function that gets all the messages posted from all users

--- src/main/java/com/google/codeu/data/Datastore.java
+++ src/main/java/com/google/codeu/data/Datastore.java
@@ -62,21 +62,50 @@ public class Datastore {
     PreparedQuery results = datastore.prepare(query);
 
     for (Entity entity : results.asIterable()) {
-      try {
-        String idString = entity.getKey().getName();
-        UUID id = UUID.fromString(idString);
-        String text = (String) entity.getProperty("text");
-        long timestamp = (long) entity.getProperty("timestamp");
-
-        Message message = new Message(id, user, text, timestamp);
-        messages.add(message);
-      } catch (Exception e) {
-        System.err.println("Error reading message.");
-        System.err.println(entity.toString());
-        e.printStackTrace();
-      }
+      readMessages(entity, messages);
     }
 
     return messages;
+  }
+
+  /**
+   * Gets all the messages posted by all the users.
+   *
+   * @return a list of messages posted by all the users, or empty list if no one has
+   *         never posted a message. List is sorted by time descending.
+   */
+  public List<Message> getAllMessages() {
+    List<Message> messages = new ArrayList<>();
+
+    Query query = 
+      new Query("Message")
+        .addSort("timestamp", SortDirection.DESCENDING);
+    PreparedQuery results = datastore.prepare(query);
+
+    for (Entity entity : results.asIterable()) {
+      readMessages(entity, messages);
+    }
+
+    return messages;
+  }
+
+  /**
+   * Reads the message of the entity and stores it in the Message List
+   */
+  private void readMessages(Entity entity, List<Message> messages) {
+    try {
+      String idString = entity.getKey().getName();
+      UUID id = UUID.fromString(idString);
+      String user = (String) entity.getProperty("user");
+      String text = (String) entity.getProperty("text");
+      long timestamp = (long) entity.getProperty("timestamp");
+
+      Message message = new Message(id, user, text, timestamp);
+      messages.add(message);
+    } catch (Exception e) {
+      System.err.println("Error reading message.");
+      System.err.println(entity.toString());
+      e.printStackTrace();
+    }
   }
 }
