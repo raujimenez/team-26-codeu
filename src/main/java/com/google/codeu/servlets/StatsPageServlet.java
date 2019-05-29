a881a46c044b51acf843494ff7262f1af05eb245
Raul Jimenez <officialrauljimenez@gmail.com>
2019-05-29 12:56:52 -0500

    Expanded Servlet logic to output count of messages in JSON format

--- src/main/java/com/google/codeu/servlets/StatsPageServlet.java
+++ src/main/java/com/google/codeu/servlets/StatsPageServlet.java
@@ -7,16 +7,32 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.codeu.data.Datastore;
+import com.google.gson.JsonObject;
+
 /**
  * Responds with a hard-coded message for testing purposes.
  */
 @WebServlet("/stats")
 public class StatsPageServlet extends HttpServlet{
   
- @Override
- public void doGet(HttpServletRequest request, HttpServletResponse response)
-   throws IOException {
+  private Datastore datastore;
   
-  response.getOutputStream().println("hello world");
- }
+  @Override
+  public void init() {
+    datastore = new Datastore();
+  }
+ 
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws IOException {
+    
+    response.setContentType("application/json");
+
+    int messageCount = datastore.getTotalMessageCount();
+
+    JsonObject jsonObject = new JsonObject();
+    jsonObject.addProperty("messageCount", messageCount);
+    response.getOutputStream().println(jsonObject.toString());
+    }
 }
