44836d65a5eeb0112fb9c081ac352ae0092179e5
Benjamín Valdez <benjamin.vr.1998@gmail.com>
2019-06-09 02:20:40 -0500

    Added image support to the user page

--- src/main/java/com/google/codeu/servlets/MessageServlet.java
+++ src/main/java/com/google/codeu/servlets/MessageServlet.java
@@ -78,9 +78,13 @@ public class MessageServlet extends HttpServlet {
     String user = userService.getCurrentUser().getEmail();
     String userEnteredContent = request.getParameter("text");
     Whitelist whitelist = Whitelist.basic();
-    String text = Jsoup.clean(userEnteredContent, whitelist);
+    String userText = Jsoup.clean(userEnteredContent, whitelist);
 
-    Message message = new Message(user, text);
+    String regex = "(https?://\\S+\\.(png|jpg))";
+    String replacement = "<img src=\"$1\" />";
+    String textWithImagesReplaced = userText.replaceAll(regex, replacement);
+
+    Message message = new Message(user, textWithImagesReplaced);
     datastore.storeMessage(message);
 
     response.sendRedirect("/user-page.html?user=" + user);
