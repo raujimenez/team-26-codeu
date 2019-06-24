60d120bb6485a58909e381193410506848fbc1ec
Benjamín Valdez <benjamin.vr.1998@gmail.com>
2019-06-24 01:48:26 -0500

    Added image analysis

--- src/main/java/com/google/codeu/servlets/FormHandlerServlet.java
+++ src/main/java/com/google/codeu/servlets/FormHandlerServlet.java
@@ -12,13 +12,22 @@ import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
 import com.google.codeu.data.Datastore;
 import com.google.codeu.data.Message;
+import com.google.cloud.vision.v1.AnnotateImageRequest;
+import com.google.cloud.vision.v1.AnnotateImageResponse;
+import com.google.cloud.vision.v1.BatchAnnotateImagesResponse;
+import com.google.cloud.vision.v1.EntityAnnotation;
+import com.google.cloud.vision.v1.Feature;
+import com.google.cloud.vision.v1.Image;
+import com.google.cloud.vision.v1.ImageAnnotatorClient;
+import com.google.protobuf.ByteString;
+import java.io.ByteArrayOutputStream;
 
 import org.jsoup.Jsoup;
 import org.jsoup.safety.Whitelist;
 
 import java.io.IOException;
 import java.util.List;
-
+import java.util.ArrayList;
 import java.util.Map;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
@@ -53,14 +62,32 @@ public class FormHandlerServlet extends HttpServlet {
         String user = userService.getCurrentUser().getEmail();
         // Get the message entered by the user.
         String text = request.getParameter("text");
-        // Get the URL of the image that the user uploaded to Blobstore.
-        String imageUrl = getUploadedFileUrl(request, "image");
+
+        // Get the BlobKey that points to the image uploaded by the user.
+        BlobKey blobKey = getBlobKey(request, "image");
+
         String userEnteredContent;
 
         //Add the html tags if the user uploaded an image
-        if (imageUrl != null) {
+        if (blobKey != null) {
+            // Get the URL of the image that the user uploaded.
+            String imageUrl = getUploadedFileUrl(blobKey);
             String tags = "<img src=\"" + imageUrl + "\"/>";
-            userEnteredContent = text + " " + tags;
+
+            ArrayList<String> labels = new ArrayList<String>();
+
+            /*
+            // Get the labels of the image that the user uploaded.
+            byte[] blobBytes = getBlobBytes(blobKey);
+            List<EntityAnnotation> imageLabels = getImageLabels(blobBytes);
+        
+            for (EntityAnnotation label : imageLabels) {
+                labels.add(label.getDescription() + ": " + label.getScore());
+            }
+            */
+            
+            String joinedLabels = String.join(" ", labels);
+            userEnteredContent = text + " " + tags + " " + joinedLabels;
         }
         else {
             userEnteredContent = text;
@@ -84,15 +111,15 @@ public class FormHandlerServlet extends HttpServlet {
     }
 
     /**
-     * Returns a URL that points to the uploaded file, or null if the user didn't
-     * upload a file.
+     * Returns the BlobKey that points to the file uploaded by the user, or null if
+     * the user didn't upload a file.
      */
-    private String getUploadedFileUrl(HttpServletRequest request, String formInputElementName) {
+    private BlobKey getBlobKey(HttpServletRequest request, String formInputElementName) {
         BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
         Map<String, List<BlobKey>> blobs = blobstoreService.getUploads(request);
         List<BlobKey> blobKeys = blobs.get("image");
 
-        // User submitted form without selecting a file, so we can't get a URL.
+        // User submitted form without selecting a file, so we can't get a BlobKey.
         // (devserver)
         if (blobKeys == null || blobKeys.isEmpty()) {
             return null;
@@ -101,7 +128,7 @@ public class FormHandlerServlet extends HttpServlet {
         // Our form only contains a single file input, so get the first index.
         BlobKey blobKey = blobKeys.get(0);
 
-        // User submitted form without selecting a file, so we can't get a URL. (live
+        // User submitted form without selecting a file, so the BlobKey is empty. (live
         // server)
         BlobInfo blobInfo = new BlobInfoFactory().loadBlobInfo(blobKey);
         if (blobInfo.getSize() == 0) {
@@ -109,13 +136,70 @@ public class FormHandlerServlet extends HttpServlet {
             return null;
         }
 
-        // We could check the validity of the file here, e.g. to make sure it's an image
-        // file
-        // https://stackoverflow.com/q/10779564/873165
+        return blobKey;
+    }
 
-        // Use ImagesService to get a URL that points to the uploaded file.
+    /**
+     * Blobstore stores files as binary data. This function retrieves the binary
+     * data stored at the BlobKey parameter.
+     */
+    private byte[] getBlobBytes(BlobKey blobKey) throws IOException {
+        BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
+        ByteArrayOutputStream outputBytes = new ByteArrayOutputStream();
+
+        int fetchSize = BlobstoreService.MAX_BLOB_FETCH_SIZE;
+        long currentByteIndex = 0;
+        boolean continueReading = true;
+        while (continueReading) {
+            // end index is inclusive, so we have to subtract 1 to get fetchSize bytes
+            byte[] b = blobstoreService.fetchData(blobKey, currentByteIndex, currentByteIndex + fetchSize - 1);
+            outputBytes.write(b);
+
+            // if we read fewer bytes than we requested, then we reached the end
+            if (b.length < fetchSize) {
+                continueReading = false;
+            }
+
+            currentByteIndex += fetchSize;
+        }
+
+        return outputBytes.toByteArray();
+    }
+
+    /**
+     * Uses the Google Cloud Vision API to generate a list of labels that apply to
+     * the image represented by the binary data stored in imgBytes.
+     */
+    private List<EntityAnnotation> getImageLabels(byte[] imgBytes) throws IOException {
+        ByteString byteString = ByteString.copyFrom(imgBytes);
+        Image image = Image.newBuilder().setContent(byteString).build();
+
+        Feature feature = Feature.newBuilder().setType(Feature.Type.LABEL_DETECTION).build();
+        AnnotateImageRequest request = AnnotateImageRequest.newBuilder().addFeatures(feature).setImage(image).build();
+        List<AnnotateImageRequest> requests = new ArrayList<>();
+        requests.add(request);
+
+        ImageAnnotatorClient client = ImageAnnotatorClient.create();
+        BatchAnnotateImagesResponse batchResponse = client.batchAnnotateImages(requests);
+        client.close();
+        List<AnnotateImageResponse> imageResponses = batchResponse.getResponsesList();
+        AnnotateImageResponse imageResponse = imageResponses.get(0);
+
+        if (imageResponse.hasError()) {
+            System.err.println("Error getting image labels: " + imageResponse.getError().getMessage());
+            return null;
+        }
+
+        return imageResponse.getLabelAnnotationsList();
+    }
+
+    /**
+     * Returns a URL that points to the uploaded file.
+     */
+    private String getUploadedFileUrl(BlobKey blobKey) {
         ImagesService imagesService = ImagesServiceFactory.getImagesService();
         ServingUrlOptions options = ServingUrlOptions.Builder.withBlobKey(blobKey);
         return imagesService.getServingUrl(options);
     }
+
 }
