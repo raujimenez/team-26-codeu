c5fd6cbe21c1accd7f1b46e55b82e104115eb4cf
lsj1124 <sjl981124@gmail.com>
2019-06-26 19:45:35 -0400

    added search place function on the map page

--- src/main/webapp/js/map-loader.js
+++ src/main/webapp/js/map-loader.js
@@ -1,7 +1,7 @@
 let map;
 /* Editable marker that displays when a user clicks in the map. */
 let editMarker;
-function createMap(){
+function createMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 38.5949, lng: -94.8923},
         zoom: 4
@@ -11,7 +11,71 @@ function createMap(){
         createMarkerForEdit(event.latLng.lat(), event.latLng.lng());
     });
     fetchMarkers();
+
+
+// Create the search box and link it to the UI element.
+    var input = document.getElementById('pac-input');
+    var searchBox = new google.maps.places.SearchBox(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener('bounds_changed', function() {
+        searchBox.setBounds(map.getBounds());
+    });
+
+    var markers = [];
+    // Listen for the event fired when the user selects a prediction and retrieve
+    // more details for that place.
+    searchBox.addListener('places_changed', function() {
+        var places = searchBox.getPlaces();
+
+        if (places.length == 0) {
+            return;
+        }
+
+        // Clear out the old markers.
+        markers.forEach(function(marker) {
+            marker.setMap(null);
+        });
+        markers = [];
+
+        // For each place, get the icon, name and location.
+        var bounds = new google.maps.LatLngBounds();
+        places.forEach(function(place) {
+            if (!place.geometry) {
+                console.log("Returned place contains no geometry");
+                return;
+            }
+            var icon = {
+                url: place.icon,
+                size: new google.maps.Size(71, 71),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(17, 34),
+                scaledSize: new google.maps.Size(25, 25)
+            };
+
+            // Create a marker for each place.
+            markers.push(new google.maps.Marker({
+                map: map,
+                icon: icon,
+                title: place.name,
+                position: place.geometry.location
+            }));
+
+            if (place.geometry.viewport) {
+                // Only geocodes have viewport.
+                bounds.union(place.geometry.viewport);
+            } else {
+                bounds.extend(place.geometry.location);
+            }
+        });
+        map.fitBounds(bounds);
+    });
+
 }
+
+
+
 /** Fetches markers from the backend and adds them to the map. */
 function fetchMarkers(){
     fetch('/markers').then((response) => {
