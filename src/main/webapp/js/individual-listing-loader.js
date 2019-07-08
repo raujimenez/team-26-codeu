46d32d92b49179f9d13121923147afc0b4e864e6
Raul Jimenez <officialrauljimenez@gmail.com>
2019-07-07 20:40:54 -0500

    added view of title and description as well as email support to listing

--- src/main/webapp/js/individual-listing-loader.js
+++ src/main/webapp/js/individual-listing-loader.js
@@ -1,9 +1,8 @@
 const urlParams = new URLSearchParams(window.location.search);
 const parameterID = urlParams.get("id");
 
-/* Pass in the listings object from fetListings to set the title*/
+/* Pass in the listings object from fetchListings to set the title */
 function setPageTitle(listing) {
-  document.getElementById('page-title').innerText = listing.title;
   document.title = listing.title;
 }
 
@@ -34,29 +33,81 @@ function fetchListings() {
  * @return {Element}
  */
 function buildListingsDiv(listing) {
-  const headerDiv = document.createElement("div");
-  headerDiv.classList.add("message-header");
-  headerDiv.classList.add("padded");
-  headerDiv.classList.add("border-bottom");
+  
+  /* contains image */
+  const pictureDiv = document.createElement("img");
+  pictureDiv.classList.add("col-lg-6");
+  pictureDiv.classList.add("col-md-6");
+  pictureDiv.classList.add("col-sm");
+  pictureDiv.src = "http://newnation.sg/wp-content/uploads/random-pic-internet-07.jpg";
+  pictureDiv.alt = "seller image";
 
-  headerDiv.appendChild(
-    document.createTextNode(listing.user + " - " + new Date(listing.timestamp))
-  );
+  /* create card-header for title and timestamp */
+  const cardHeader = document.createElement("div");
+  cardHeader.classList.add("card-header");
+  
+  /* formatted using table to better align items */
+  const tableDiv = document.createElement("table");
+  tableDiv.classList.add("table");
+  tableDiv.classList.add("table-borderless");
+  tableDiv.setAttribute("style", "margin: 0");
+  
+  const tableBody = document.createElement("tbody");
+  const tableRow = document.createElement("tr");
+  
+  const listingTitle = document.createElement("td");
+  listingTitle.innerHTML = '<font size=+1.5>' + listing.title + '</font>';
+  const listingDate = document.createElement("td");
+  listingDate.classList.add("text-right");
+  listingDate.innerHTML = '<font class="text-right text-muted" size="-1">' + new Date(listing.timestamp).toLocaleTimeString('en-US') + '</font>';
 
-  const bodyDiv = document.createElement("div");
-  bodyDiv.classList.add("message-body");
-  bodyDiv.classList.add("padded");
-  bodyDiv.classList.add("border-left");
-  bodyDiv.classList.add("border-bottom");
-  bodyDiv.classList.add("border-right");
-  bodyDiv.innerHTML =  listing.title + listing.text;
+  tableRow.appendChild(listingTitle);
+  tableRow.appendChild(listingDate);
 
+  tableBody.appendChild(tableRow);
+  tableDiv.appendChild(tableBody);
+  
+  cardHeader.appendChild(tableDiv);
+
+  /* contains description of listing */
+  const cardBody = document.createElement("div");
+  cardBody.classList.add("card-body");
+  cardBody.innerHTML = listing.text;
+
+  const cardFooter = document.createElement("div");
+  cardFooter.classList.add("card-footer");
+  cardFooter.classList.add("text-center");
+
+  /* Creates contact information */
+  const contactDiv = document.createElement("a");
+  contactDiv.classList.add("btn");
+  contactDiv.classList.add("btn-primary");
+  contactDiv.href = "mailto:" + listing.user;
+  contactDiv.innerHTML = "Send them an Email";
+  
+  cardFooter.appendChild(contactDiv);
+
+  /* Container that formats title, description, and contact information of listing */
+  const infoDiv = document.createElement("div");
+  infoDiv.classList.add("card");
+  infoDiv.classList.add("h-100");
+  infoDiv.appendChild(cardHeader);
+  infoDiv.appendChild(cardBody);
+  infoDiv.appendChild(cardFooter);
+  
+  /* needed to make listing info same size as image */
+  const cardContainer = document.createElement("div");
+  cardContainer.classList.add("col-lg-6");
+  cardContainer.classList.add("col-md-6");
+  cardContainer.classList.add("col-sm");
+
+  cardContainer.appendChild(infoDiv);
+
+  /* contains all information in a listing */
   const listingDiv = document.createElement("div");
-  listingDiv.classList.add("message-div");
-  listingDiv.classList.add("rounded");
-  listingDiv.classList.add("panel");
-  listingDiv.appendChild(headerDiv);
-  listingDiv.appendChild(bodyDiv);
+  listingDiv.classList.add("card-deck");
+  listingDiv.appendChild(pictureDiv);
+  listingDiv.appendChild(cardContainer);
 
   return listingDiv;
 }
